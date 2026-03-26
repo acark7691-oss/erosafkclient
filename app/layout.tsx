27164751167129f975b7a87c54147fefca9b013c
acark7691-oss <acark7691@gmail.com>
2026-03-26 19:49:35 +0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Eros AFK Client",
   description: "Minecraft AFK Bot Control Panel",
+  
+  // Google Site Doğrulama (Eros AFK)
+  verification: {
+    google: "cbSHSusT62qjzQfCQ2dRGDM0URlnTaaUamIWSK-j7Ew",
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
